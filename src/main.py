a47775065865460e381041240ad301bbef666a4f
Артур Батыршин <Artur20.06.2000@icloud.com>
2026-05-26 02:03:18 +0300

    change structure project

--- src/main.py
+++ src/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI, Query, Body
 import uvicorn
 
-from hotels import router as router_hotel
+import sys
+from pathlib import Path
+
+sys.path.append(str(Path(__file__).parent.parent))
+
+from src.api.hotels import router as router_hotel
 
 app = FastAPI()
 
