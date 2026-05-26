74ca3f1309aae48271de8bc07a2afde4bf248f05
Артур Батыршин <Artur20.06.2000@icloud.com>
2026-05-26 12:26:17 +0300

    add model rooms.py & new migration

---
+++ src/migration/versions/032f32c15bb1_rooms_migration.py
@@ -0,0 +1,35 @@
+"""rooms migration
+
+Revision ID: 032f32c15bb1
+Revises: e6553e3f1600
+Create Date: 2026-05-26 12:24:36.720321
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '032f32c15bb1'
+down_revision: Union[str, Sequence[str], None] = 'e6553e3f1600'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.create_table('rooms',
+        sa.Column('id', sa.Integer(), nullable=False),
+        sa.Column('hotel_id', sa.Integer(), nullable=False),
+        sa.Column('title', sa.Integer(), nullable=False),
+        sa.Column('description', sa.String(), nullable=True),
+        sa.Column('price', sa.Integer(), nullable=False),
+        sa.Column('quantity', sa.Integer(), nullable=False),
+        sa.ForeignKeyConstraint(['hotel_id'], ['hotels.id'], ),
+        sa.PrimaryKeyConstraint('id')
+    )
+
+
+def downgrade() -> None:
+    op.drop_table('rooms')
