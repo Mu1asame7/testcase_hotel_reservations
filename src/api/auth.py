013dca97e6e6115e0b7f39817756deaa558f27f7
Артур Батыршин <Artur20.06.2000@icloud.com>
2026-05-29 07:56:38 +0300

    add login endpoint

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,6 +1,9 @@
-from fastapi import APIRouter
+from datetime import datetime, timedelta, timezone
+
+from fastapi import APIRouter, HTTPException, Response
 
 from pwdlib import PasswordHash
+import jwt
 
 from src.database import async_session_maker
 from repositories.users import UsersRepository
@@ -9,6 +12,20 @@ from src.schemas.users import UserRequestAdd, UserAdd
 router = APIRouter(prefix="/auth", tags=["Авторизация и аутентификация"])
 
 password_hash = PasswordHash.recommended()
+SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+def create_access_token(data: dict) -> str:
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+def verify_password(plain_password, hashed_password):
+    return password_hash.verify(plain_password, hashed_password)
 
 
 @router.post("/register")
@@ -22,3 +39,19 @@ async def register_user(
         await session.commit()
 
     return {"status": "OK"}
+
+
+@router.post("/login")
+async def login_user(
+        data: UserRequestAdd,
+        response: Response,
+):
+    async with async_session_maker() as session:
+        user = await UsersRepository(session).get_user_with_hashed_password(email=data.email)
+        if not user:
+            raise HTTPException(status_code=401, detail="User not found")
+        if not verify_password(data.password, user.hashed_password):
+            raise HTTPException(status_code=401, detail="Incorrect password")
+        access_token = create_access_token({"user_id": user.id})
+        response.set_cookie("access_token", access_token)
+        return {"access_token": access_token, "token_type": "bearer"}
--- src/repositories/users.py
+++ src/repositories/users.py
@@ -1,4 +1,7 @@
-from src.schemas.users import User
+from pydantic import EmailStr
+from sqlalchemy import select
+
+from src.schemas.users import User, UserWithHashedPassword
 from src.models.users import UsersORM
 from src.repositories.base import BaseRepository
 
@@ -6,3 +9,9 @@ from src.repositories.base import BaseRepository
 class UsersRepository(BaseRepository):
     model = UsersORM
     schema = User
+
+    async def get_user_with_hashed_password(self, email: EmailStr):
+        query = select(self.model).filter_by(email=email)
+        result = await self.session.execute(query)
+        model = result.scalars().one()
+        return UserWithHashedPassword.model_validate(model)
