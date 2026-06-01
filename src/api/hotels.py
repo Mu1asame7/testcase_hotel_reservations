ff24342bdb7f8d9ffd20f2ea389ca30507b1ff17
Артур Батыршин <Artur20.06.2000@icloud.com>
2026-06-01 06:18:59 +0300

    add get_filtered_by_time in HotelsRepo

--- src/api/hotels.py
+++ src/api/hotels.py
@@ -1,7 +1,7 @@
+from datetime import date
+
 from fastapi import Query, APIRouter
 
-from repositories.hotels import HotelsRepository
-from src.database import async_session_maker
 from src.api.dependencies import PaginationDep, DBDep
 from src.schemas.hotels import HotelAdd, HotelPATCH
 
@@ -14,13 +14,12 @@ async def get_hotels(
     db: DBDep,
     title: str | None = Query(default=None),
     location: str | None = Query(default=None),
+    date_from: date = Query(example="2024-08-01"),
+    date_to: date = Query(example="2024-08-10"),
 ):
-    per_page = pagination.per_page or 5
-    return await db.hotels.get_all(
-        location=location,
-        title=title,
-        limit=per_page or 5,
-        offset=per_page * (pagination.page - 1)
+    return await db.hotels.get_filtered_by_time(
+        date_from=date_from,
+        date_to=date_to
     )
 
 
--- src/repositories/hotels.py
+++ src/repositories/hotels.py
@@ -1,5 +1,9 @@
+from datetime import date
+
 from sqlalchemy import select, func
 
+from src.models.rooms import RoomsORM
+from src.repositories.utils import rooms_ids_for_booking
 from src.repositories.base import BaseRepository
 from src.schemas.hotels import Hotel
 from src.models.hotels import HotelsORM
@@ -31,3 +35,16 @@ class HotelsRepository(BaseRepository):
             result = await self.session.execute(query)
 
             return [Hotel.model_validate(hotel, from_attributes=True) for hotel in result.scalars().all()]
+
+    async def get_filtered_by_time(
+            self,
+            date_from: date,
+            date_to: date,
+    ):
+        rooms_ids_to_get = rooms_ids_for_booking(date_from=date_from, date_to=date_to)
+        hotels_ids = (
+            select(RoomsORM.hotel_id)
+            .select_from(RoomsORM)
+            .filter(RoomsORM.id.in_(rooms_ids_to_get))
+        )
+        return  await self.get_filtered(HotelsORM.id.in_(hotels_ids))
