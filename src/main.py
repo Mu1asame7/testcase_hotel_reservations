0c445f3e6b5d76d45add794272e62ed3b25853d7
Артур Батыршин <Artur20.06.2000@icloud.com>
2026-06-05 10:03:26 +0300

    add fastapy infinite async task

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import asyncio
 import sys
 from contextlib import asynccontextmanager
 from pathlib import Path
@@ -10,6 +11,8 @@ from fastapi_cache.backends.redis import RedisBackend
 
 sys.path.append(str(Path(__file__).parent.parent))
 
+from src.api.dependencies import get_db
+
 from src.api.hotels import router as router_hotels
 from src.api.auth import router as router_auth
 from src.api.rooms import router as router_rooms
@@ -19,8 +22,21 @@ from src.api.images import router as router_images
 from src.init import redis_manager
 
 
+async def send_emails_bookings_today_checkin():
+    async for db in get_db():
+        bookings = await db.bookings.get_bookings_with_today_checkin()
+        print(bookings)
+
+
+async def run_send_email_regularly():
+    while True:
+        await send_emails_bookings_today_checkin()
+        await asyncio.sleep(5)
+
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    asyncio.create_task(run_send_email_regularly())
     await redis_manager.connect()
 
     FastAPICache.init(RedisBackend(redis_manager.redis), prefix="fastapi_cache")
