308c1ade8cb6fb4c9920855765d3e275ddfd535d
Артур Батыршин <Artur20.06.2000@icloud.com>
2026-06-10 10:14:19 +0300

    add mock data to database

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,16 @@
+import json
+
 import pytest
 from httpx import AsyncClient, ASGITransport
+from pydantic import BaseModel
 
-from config import settings
+from src.config import settings
+from src.schemas.hotels import HotelAdd
+from src.schemas.rooms import RoomAdd
 from src.main import app
-from src.database import Base, engine_null_pool
+from src.database import Base, engine_null_pool, async_session_maker_null_pool
 from src.models import *
+from src.utils.db_manager import DBManager
 
 
 @pytest.fixture(scope="session", autouse=True)
@@ -17,6 +23,19 @@ async def setup_database(check_mode):
     async with engine_null_pool.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all) # type: ignore
         await conn.run_sync(Base.metadata.create_all) # type: ignore
+
+    with open("tests/mock_hotels.json", "r", encoding="utf-8") as file_hotels:
+        hotels = json.load(file_hotels)
+    with open("tests/mock_rooms.json", "r", encoding="utf-8") as file_rooms:
+        rooms = json.load(file_rooms)
+
+    hotels = [HotelAdd.model_validate(hotel) for hotel in hotels]
+    rooms = [RoomAdd.model_validate(room) for room in rooms]
+
+    async with DBManager(session_factory=async_session_maker_null_pool) as db:
+        await db.hotels.add_bulk(hotels)
+        await db.rooms.add_bulk(rooms)
+        await db.commit()
 
 
 @pytest.fixture(scope="session", autouse=True)
